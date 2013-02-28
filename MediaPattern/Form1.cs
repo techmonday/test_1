a7dd1028ca45468396d907afde31f794b74a2622
techmonday <techmonday@gmail.com>
2013-02-28 00:37:59 -0800

    upload mediaPattern

---
+++ MediaPattern/Form1.cs
@@ -0,0 +1,29 @@
+﻿// http://ashishkhandelwal.arkutil.com/design-patterns-technical/quick-and-short-mediator-pattern/
+
+using System;
+using System.Windows.Forms;
+
+namespace MediaPattern
+{
+    public partial class Form1 : Form
+    {
+        Mediator mediator = new Mediator();
+
+        public Form1()
+        {
+            InitializeComponent();
+            mediator.RegisterSearch(button1);
+            mediator.RegisterView(button2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mediator.Search();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mediator.View();
+        }
+    }
+}
---
+++ MediaPattern/Mediator.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+
+namespace MediaPattern
+{
+    //Col
+    public interface IMediator
+    {
+        void View();
+        void Search();
+        void RegisterView(Button v);
+        void RegisterSearch(Button s);
+    }
+
+
+    //Concrete mediator
+    public class Mediator : IMediator {
+ 
+        Button btnView;
+        Button btnSearch;
+ 
+        public void RegisterView(Button v) 
+        {
+            btnView = v;
+        }
+ 
+        public void RegisterSearch(Button s) 
+        {
+            btnSearch = s;
+        }
+  
+        public void View() 
+        {
+            btnView.Enabled = false;
+            btnSearch.Enabled = true;
+            //show.setText("viewing...");
+        }
+ 
+        public void Search() 
+        {
+            btnSearch.Enabled = false;
+            btnView.Enabled = true;
+            //show.setText("searching...");
+        }
+    }
+}
